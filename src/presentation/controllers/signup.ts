647740f66c237933c0a9d55149b8db66a33ab493
Marcos Henrique <marcoshenriqueh393@gmail.com>
2025-02-18 20:45:42 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,14 @@ import type { HttpRequest, HttpResponse } from "../protocols/http";
 
 export class SignUpController {
 	handle(httpRequest: HttpRequest): HttpResponse {
-		if (!httpRequest.body.name) {
-			return badRequest(new MissingPararmError("name"));
+		const requiredFields = ["name", "email"];
+
+		for (const field of requiredFields) {
+			if (!httpRequest.body[field]) {
+				return badRequest(new MissingPararmError(field));
+			}
 		}
-		if (!httpRequest.body.email) {
-			return badRequest(new MissingPararmError("email"));
-		}
+
 		return {
 			statusCode: 200,
 			body: { message: "tudo certo" },
